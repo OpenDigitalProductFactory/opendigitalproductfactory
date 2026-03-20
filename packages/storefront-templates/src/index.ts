7a5e1404fa9a7fa30fc7a4f8612853dbc5e36ecc
Mark Bodman <markdbodman@gmail.com>
2026-03-19 20:47:08 -0500

    feat(storefront-templates): scaffold package with core types

---
+++ packages/storefront-templates/src/index.ts
@@ -0,0 +1,11 @@
+// Re-export all types from types.ts
+export type {
+  CtaType,
+  PriceType,
+  SectionType,
+  ArchetypeCategory,
+  FormField,
+  ItemTemplate,
+  SectionTemplate,
+  ArchetypeDefinition,
+} from "./types";
